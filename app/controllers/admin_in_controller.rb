afe50940411a14bd8576987021df9f9ddad8d83f
Sheku1 <david324567@gmail.com>
2017-03-07 22:24:03 -0800

    agregue que se mostraran eventos en tablas de admin, y ademas que se vieran en base a la fehca de la semana donde se creo el evento.. si editas tmb se editan en la tabla

--- app/controllers/admin_in_controller.rb
+++ app/controllers/admin_in_controller.rb
@@ -2,10 +2,14 @@ class AdminInController < ApplicationController
 	before_action :authenticate_admin!
 	def home
 		@admin = current_admin
+		@eventos = Evento.all
+		@weekstart = Date.current
+		@weekdos = @weekstart.at_beginning_of_week+7.days
 	end
 
 	def admins
 		@admin = current_admin
+		@eventos = Evento.all
 		@admins = Admin.paginate(:page => params[:page], :per_page => 10)
 	end
 
@@ -17,6 +21,7 @@ class AdminInController < ApplicationController
 
 	def grupos
 		@admin = current_admin
+		@eventos = Evento.all
 		@admins = Admin.paginate(:page => params[:page], :per_page => 10)
 	end
 
