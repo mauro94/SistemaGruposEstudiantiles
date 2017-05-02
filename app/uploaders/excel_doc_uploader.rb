86ee148c4510b16d019c34f32cab1407c757f1c1
Mauro Amarante <mauro.amarante94@gmail.com>
2017-05-02 01:31:59 -0500

    excel file upload fix

--- app/uploaders/excel_doc_uploader.rb
+++ app/uploaders/excel_doc_uploader.rb
@@ -37,7 +37,7 @@ class ExcelDocUploader < CarrierWave::Uploader::Base
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_whitelist
-    %w(xlsm)
+    %w(xlsx)
   end
 
   # Override the filename of the uploaded files:
